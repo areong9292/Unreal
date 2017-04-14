0040b3546c271595a0b114829ef152342925210f
areong9292 <areong9292@gmail.com>
2017-04-14 14:52:28 +0900

    Grab pressed and released

--- BuildingEscape/Source/BuildingEscape/Grabber.h
+++ BuildingEscape/Source/BuildingEscape/Grabber.h
@@ -25,6 +25,14 @@ public:
 private:
 	// How far ahead of the player can we reach in cm
 	float Reach=100.f;
-		
+	UPhysicsHandleComponent* PhysicsHandle = nullptr;
+
+	UInputComponent* InputComponent = nullptr;
+
+	// Ray-cast and grab what's in reach
+	void Grab();
+
+	// Called when grab is released
+	void Release();
 	
 };
